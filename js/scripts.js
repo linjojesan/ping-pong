ed1a82f5b47a646c9f6e98300ad1064a058b96cf
John Linneman <Guest@Epicodus-3A.local>
2017-03-19 19:15:56 -0700

    added business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,37 @@
+//business logic
+var pong = function(count) {
+  var numberRange = [];
+  for (var n = 1; n <= count; number++) {
+    if (n % 3 === 0) {
+      numberRange.push("Ping");
+    } else if ( n % 5 === 0 ) {
+      numberRange.push("Pong")
+    }  else if (n % 15 === 0) {
+      numberRange.push("Ping Pong")
+    }
+      else {
+      numberRange.push(n);
+    };
+  };
+    return numberRange;
+
+
+
+
+
+
+
+
 //user interface logic
-$(function blink() {
-  $('#blinky').fadeOut(500).fadeIn(500, blink); 
-})();
+$(document).ready(function() {
+
+  $("form").submit(function(event) {
+
+    var count =parseInt($("#enter").val());
+    console.log();
+
+  };
+};
+// $(function blink() {
+//   $('#blinky').fadeOut(500).fadeIn(500, blink);
+// })();
