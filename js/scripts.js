a8a429573bc723a393e9c9aaeb83047dcd1e2257
linjojesan <urielpro78@gmail.com>
2017-03-28 13:21:26 -0700

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,54 @@
-//business logic
-var pong = function(count) {
-  var numberRange = [];
-  for (var n = 1; n <= count; number++) {
-    if (n % 3 === 0) {
-      numberRange.push("Ping");
-    } else if ( n % 5 === 0 ) {
-      numberRange.push("Pong")
-    }  else if (n % 15 === 0) {
-      numberRange.push("Ping Pong")
-    }
-      else {
-      numberRange.push(n);
-    };
-  };
-    return numberRange;
+var numberFun = [];
 
-
-
-
-
-
-
-
-//user interface logic
-$(document).ready(function() {
-
-  $("form").submit(function(event) {
-
-    var count =parseInt($("#enter").val());
-    console.log();
-
-  };
+var ping = function(number) {
+  if (number % 3 === 0 && number !== 0) {
+    return true;
+  }
 };
+
+var pong = function(number) {
+  if (number % 5 === 0 && number !== 0) {
+    return true;
+  }
+};
+
+function pingPong(userNumber) {
+  for ( var i = 1; i <= userNumber; i += 1 ) {
+    if (ping(i) && pong(i)) {
+      numberFun.push("pingpong");
+    } else if (ping(i)) {
+      numberFun.push("ping");
+    } else if (pong(i)) {
+      numberFun.push("pong");
+    } else
+      numberFun.push(i);
+  }
+}
+
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    var userNumber = $("input#yournumber").val();
+
+    if (userNumber <= 0) {
+      $("#warning").html("<p>Please Enter a Number Above 1. Thank you!</p>");
+    } else {
+      pingPong(userNumber);
+    }
+
+    numberFun.forEach(function(number) {
+      $(".numbers").append("<li>" + number + "</li>");
+    });
+  });
+});
+//
+
+
+    // console.log(count);
+
+
 // $(function blink() {
 //   $('#blinky').fadeOut(500).fadeIn(500, blink);
 // })();
